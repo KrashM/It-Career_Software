062c02f6cca9e3c56030d6a21cbafb0de6c4a985
KrashM <hristo_mesi1990@abv.bg>
2020-02-13 16:30:39 +0200

    Made the connection!!!

--- Software_Project/Handler.cs
+++ Software_Project/Handler.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using MySql.Data.MySqlClient;
+using MySql.Data;
 
 namespace Software_Project.Handlers {
 
@@ -12,12 +13,16 @@ namespace Software_Project.Handlers {
 
         private void Connect() {
 
-            string constr;
-            SqlConnection conn;
-            constr = @"Data Source=localhost;Initial Catalog=Main;User ID=root;Password=root";
-            conn = new SqlConnection(constr);
+            MySqlConnection conn = new MySqlConnection("Server=localhost;Database=main;Uid=root;Pwd=root;");
             conn.Open();
             System.Console.WriteLine("Connection Open!");
+            var stm = "use main;create table users(username varchar(50));insert into users values('Hristo'),('Pesho');";
+            var cmd = new MySqlCommand(stm, conn);
+            cmd.ExecuteScalar();
+            stm = "select * from users;";
+            cmd = new MySqlCommand(stm, conn);
+            var result = cmd.ExecuteScalar().ToString();
+            System.Console.WriteLine($"{result}");
             conn.Close();
 
         }
