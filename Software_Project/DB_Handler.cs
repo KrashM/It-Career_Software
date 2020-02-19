c08f16dc8d97f3f730ee5257dd7c8e0bfc934885
KrashM <hristo_mesi1990@abv.bg>
2020-02-19 19:11:21 +0200

    Added Registration, LogIn, basic menu and Hasher for the passwords.

---
+++ Software_Project/DB_Handler.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using Software_Project.Hasher;
+
+namespace Software_Project.Database_Handlers {
+
+    class DB_Handler {
+
+        private MySqlConnection conn = new MySqlConnection("Server=localhost;Uid=root;Pwd=root;");
+
+        public DB_Handler() {
+
+            Connect();
+
+        }
+
+        private void Connect() {
+
+            conn.Open();
+            MySqlCommand stm = new MySqlCommand("create database if not exists main; use main;", conn);
+            stm.ExecuteScalar();
+            Create_Tables();
+
+        }
+
+        private void Create_Tables(){
+
+            MySqlCommand cmd = new MySqlCommand("create table if not exists users(username varchar(50), password varchar(100));", conn);
+            cmd.ExecuteScalar();
+
+        }
+
+        private void Input(ref string username, ref string password){
+
+            System.Console.WriteLine("Give me your username:");
+            username = System.Console.ReadLine();
+            System.Console.WriteLine("Give me your password:");
+            password = System.Console.ReadLine();
+
+        }
+
+        public void Create_User(){
+
+            string username = "", password = "";
+            Input(ref username, ref password);
+            
+            var hash = Password_Hasher.Hash(password);
+            var result = Password_Hasher.Verify(password, hash);
+
+            //System.Console.WriteLine($"{username} => {hash}");
+
+            MySqlCommand cmd = new MySqlCommand($"insert into users values('{username}', '{hash}');", conn);
+            cmd.ExecuteScalar();
+
+        }
+
+        public void LogIn_User(){
+
+            string username = "", password = "";
+            Input(ref username, ref password);
+            string hashed_pass = (string)new MySqlCommand($"select password from users where username = '{username}';", conn).ExecuteScalar();
+            //System.Console.WriteLine($"{username} => {password} => {hashed_pass}");
+
+            if(Password_Hasher.Verify(password, hashed_pass))
+                System.Console.WriteLine("Successfully verified");
+            else
+                System.Console.WriteLine("Access denied");
+
+        }
+        
+        public void Close_Connection(){
+
+            conn.Close();
+
+        }
+
+    }
+
+}
--- Software_Project/Program.cs
+++ Software_Project/Program.cs
@@ -1,15 +1,49 @@
-﻿using Software_Project.Handlers;
+﻿using Software_Project.Database_Handlers;
 
 namespace Software_Project {
 
     class Program {
 
+        private static DB_Handler handler;
+
+        private static Program instance = new Program();
+        public static Program Instance { get { return instance; } }
+
         static void Main() {
 
-            Handler handler = new Handler();
+            handler = new DB_Handler();
+            Instance.Initiate();
             handler.Close_Connection();
 
         }
+
+        private void Initiate(){
+
+            while(true){
+
+                var selection = Menu();
+                if(selection == 1)
+                    Register();
+                if(selection == 2)
+                    LogIn();
+                if(selection == 0)
+                    break;
+
+            }
+
+        }
+
+        private int Menu(){
+
+            System.Console.WriteLine("1.Register\n2.Log in\n3.Exit");
+            var input = int.Parse(System.Console.ReadLine());
+            return (input == 1 && input == 2) ? input : 0;
+
+        }
+
+        private void Register(){ handler.Create_User(); }
+
+        private void LogIn(){ handler.LogIn_User(); }
 
     }
 
