725575af199bf7f35dbf5fd4a9a9a38cd60900d0
Krash <HristoKanevKrash@gmail.com>
2020-04-17 20:05:57 +0300

    Connection is ready.

---
+++ Software_Project/Data/Models/Product.cs
@@ -0,0 +1,12 @@
+namespace Software_Project.Data.Models{
+
+    public class Product{
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+
+}
