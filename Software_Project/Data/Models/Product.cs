8d0cec3dd65d15542a2e8caa4b233bf635064a2c
Krash <HristoKanevKrash@gmail.com>
2020-04-19 15:47:08 +0300

    Merged with Valeri

--- Software_Project/Data/Models/Product.cs
+++ Software_Project/Data/Models/Product.cs
@@ -3,10 +3,18 @@ namespace Software_Project.Data.Models{
     public class Product{
 
         public int Id { get; set; }
+<<<<<<< HEAD
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Stock { get; set; }
 
+=======
+        public int Stock { get; set; } 
+        public string Name { get; set; }
+        public string Parameters { get; set; }
+        public decimal Price { get; set; }
+      
+>>>>>>> 5622059661db793a63adff5321c870a22254e804
     }
 
 }
